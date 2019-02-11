101d9d50c372fc85f750359744dd2a8bbff2d0b7
jvclbr <47107983+jvclbr@users.noreply.github.com>
2019-02-11 11:43:24 -0200

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -15,6 +15,8 @@ class DeckOfCards:
     """
     Definition of the deck and the values of the cards used
     """
+    max_cards = 52   
+    
     def __init__(self):
         self.numeros = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J',
                         'Q', 'K']
@@ -33,37 +35,68 @@ class DeckOfCards:
         Get the value of a single card
         """
         return self.valores[self.deck.index(card)]
-
+    
+    def deal_cards(self):
+        """
+        randomly selects a deck card
+        """
+        aux = random.randint(0, self.max_cards)
+        self.max_cards -= 1
+        card = self.deck[aux]
+        self.deck.pop(aux)
+        print(f" A carta recebida foi: {card}")
+        return card
 
 class Hand(DeckOfCards):
     """
     Stores the hand of the player
     """
     def __init__(self):
+        DeckOfCards.__init__(self)
         self.cards = []
         self.totalValue = 0
 
     def __str__(self):
         return str(self.cards)
-
+    
+    def sum_hand(self, cards):
+        """
+        return the number of elephants playing the game
+        """   
+        self.totalValue = 0
+        have_a = False
+        num_of_a = 0
+        for card in cards:     
+            if "A" in card:
+                have_a = True
+                num_of_a += 1
+            else:
+                self.totalValue += DeckOfCards.value(self,card) 
+                
+        if have_a == True:
+            cont = 0
+            while cont < num_of_a:
+                self.totalValue += 11
+                if self.totalValue > 21:
+                    self.totalValue-= 10
+                cont += 1
+        
     def add_card(self, card):
         self.cards.append(card)
-
+        self.sum_hand(self.cards)
+        print(f" o valor da mão é: {self.totalValue}")
+    
 
 if __name__ == "__main__":
-    MY_DECK = DeckOfCards()
-    MY_HAND = Hand()
-    print(MY_DECK)
-    print(MY_DECK.valores)
-    a = random.randint(0, 52)
-    print(f'{MY_DECK.deck[a]} and  {MY_DECK.value(MY_DECK.deck[a])}')
-    print(MY_DECK.value('A♦'))
-    print(MY_HAND)
-    MY_HAND.add_card(MY_DECK.deck[a])
-    print(MY_HAND)
-    a = random.randint(0, 52)
-    MY_HAND.add_card(MY_DECK.deck[a])
-    print(MY_HAND)
-    a = random.randint(0, 52)
-    MY_HAND.add_card(MY_DECK.deck[a])
-    print(MY_HAND)
+    my_deck = DeckOfCards()
+    my_hand = Hand()
+    aux = 0
+    print(f"meu deck inical é: {my_deck}")
+    print(f"minha Mão inical é: {my_hand}")
+    while aux< 10:
+        
+        my_hand.add_card(my_deck.deal_cards())
+        aux += 1
+        
+    print(f"meu deck final é: {my_deck}")
+    print(f"minha Mão final é: {my_hand}")
